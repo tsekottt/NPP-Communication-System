516e2c95d208cbccedf37e42754b7ba1533f98ff
tsekottt <tsekot.mariana@lll.kpi.ua>
2025-05-25 23:46:58 +0300

    Added Implementation Of Repositories

---
+++ NPP-CS/DAL/Repositories/Impl/RequestRepository.cs
@@ -0,0 +1,25 @@
+﻿using DAL.EF;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Impl
+{
+    public class RequestRepository
+          : BaseRepository<Request>, IRequestRepository
+    {
+
+        public RequestRepository(NPPCSContext context)
+            : base(context)
+        {
+        }
+        public IEnumerable<Request> GetByUserId(int userId)
+        {
+            return _set.Where(r => r.UserId == userId).ToList();
+        }
+    }
+}
---
+++ NPP-CS/DAL/Repositories/Impl/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using DAL.EF;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Impl
+{
+    public class UserRepository
+          : BaseRepository<User>, IUserRepository
+    {
+
+        public UserRepository(NPPCSContext context)
+            : base(context)
+        {
+        }
+        public User GetByUsername(string username)
+        {
+            return _set.FirstOrDefault(u => u.Username == username);
+        }
+    }
+}
