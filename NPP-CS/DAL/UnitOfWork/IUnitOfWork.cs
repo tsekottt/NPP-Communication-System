24838d7d2d2b1b9321fb81f899ab9771ca6736f3
tsekottt <tsekot.mariana@lll.kpi.ua>
2025-05-25 23:50:33 +0300

    Added UnitOfWork

---
+++ NPP-CS/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        IRequestRepository Requests { get; }
+        void Save();
+    }
+}
